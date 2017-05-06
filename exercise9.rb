85bfaa57ce2a803c7170aef0744f0359f98e6dc5
ashrafmansur <ashraf261090@gmail.com>
2017-05-06 15:47:50 +0800

    completed exercise9

--- exercise9.rb
+++ exercise9.rb
@@ -5,8 +5,6 @@ def max_profit(nums)
 	selling_prices = []
 	length = nums.length
 
-
-
 #buying prices
 	for i in 0..(length-2)
 		if i == 0
@@ -20,32 +18,24 @@ def max_profit(nums)
 		end
 	
 	end
-	puts buying_prices
-
-
-
-
-
-
-
-
-
 
 #selling prices
-
-
-
-
-
-
-
-
+	for i in 1..(length-2)			
+		if (nums[i] > nums[i+1]) && (nums[i] >= nums[i-1])
+			selling_prices.push(nums[i])
+		end
+	end
+		
 #finding the difference between buying and selling
+	if buying_prices.length > selling_prices.length
+		buying_prices.pop
+	end
 
-
-
-
+	sum_of_buying = buying_prices.inject(:+)
+	sum_of_selling = selling_prices.inject(:+)
+	profit = (sum_of_selling - sum_of_buying)
+	puts profit
 end
 
 puts max_profit([7,5,2,9,1,6,2])
-#puts max_profit([2,1,4,1])
+puts max_profit([5,6,4,1,2,7])
